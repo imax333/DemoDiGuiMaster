d95fdb24504e4b121c06aabe90b2d7ec8a82afb1
peter <peter@qq.com>
2019-06-26 22:24:20 +0800

    格式化

--- src/main/java/com/atpingan/DiGuiDemo.java
+++ src/main/java/com/atpingan/DiGuiDemo.java
@@ -3,26 +3,26 @@ package com.atpingan;
 public class DiGuiDemo {
 
 	public static void main(String[] args) {
-		
+
 		int diGui = DiGui(5);
-		System.out.println("  阶乘为 :" + diGui );
+		System.out.println("  阶乘为 :" + diGui);
 		System.out.println("this  is test 1 ");
 		System.out.println("this is test 2 ");
 	}
-	
-	
+
 	public static int DiGui(int num) {
-		if(num == 1) {
+		if (num == 1) {
 			return 1;
-		}else {
-			return DiGui(num -1) * num; 
+		} else {
+			return DiGui(num - 1) * num;
 		}
-	}  
-	public void say(String sss ) {
+	}
+
+	public void say(String sss) {
 		System.out.println(" this is say ");
 	}
-	
-public void say() {
+
+	public void say() {
 		System.out.println("hello ");
 	}
 
